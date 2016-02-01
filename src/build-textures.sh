ca5a754de7a77d68b50e71040974fc23fd0f03d5
xhaggi <sascha.woo@gmail.com>
2016-02-01 18:46:31 +0100

    [cleanup] removes obsolete shell-script

--- src/build-textures.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./TexturePacker -input addons/skin.xperience1080x/media -output addons/skin.xperience1080x/media/Textures.xbt
-find ./addons/skin.xperience1080x/media ! -name 'Textures.xbt' -type df -exec rm -R {} \;
-
-./TexturePacker -input addons/skin.droid/media -output addons/skin.droid/media/Textures.xbt
-find ./addons/skin.droid/media ! -name 'Textures.xbt' -type df -exec rm -R {} \;
